5ee9b2fbd40b46c118588af89f7156a974de28f3
Johan <johangeorge2002@gmail.com>
2024-09-23 01:42:41 +0530

    changes to .env

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors()); // To allow cross-origin requests
 app.use(express.json()); // To parse JSON bodies
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/taskdb')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
